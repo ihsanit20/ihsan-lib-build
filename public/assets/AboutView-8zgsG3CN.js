089c4f4978e6d2a0ab2a1fcf7b34cdb895078e6d
msisaif <msi132043@gmail.com>
2025-04-04 17:48:27 +0600

    Build: Remove unused AboutView and LoaderView components & add .htaccess and update npm dependencies

--- public/assets/AboutView-8zgsG3CN.js
+++
@@ -1,1 +0,0 @@
-import{g as o,c as s,a as t,o as a}from"./index-B9QrrNei.js";const n={},c={class:"about"};function r(i,e){return a(),s("div",c,e[0]||(e[0]=[t("h1",null,"This is an about page",-1)]))}const l=o(n,[["render",r]]);export{l as default};
